2e7b09ff9ade6fd8242ebc72dc6d4e4909c7db93
Leebal13 <98734053+Leebal13@users.noreply.github.com>
2023-07-04 21:34:43 +0900

    name validation in japanese ケー

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   validates :kana_first_name, presence: true
   validates :kana_last_name, presence: true
 
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters" }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters" }
-  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "must be full-width characters" }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "must be full-width characters" }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "must be full-width katakana characters" }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "must be full-width katakana characters" }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,message: "must be valid" }
 
   validates :birthdate, presence: true
