f8a54facb1e8986ad46255e8dc0f8dd0e23251a3
Leebal13 <98734053+Leebal13@users.noreply.github.com>
2023-07-09 17:35:31 +0900

    コントローラーでデータを取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    # @item = Item.order("created_at DESC")
+    @item = Item.find(1) 
   end
 
   def move_to_index
