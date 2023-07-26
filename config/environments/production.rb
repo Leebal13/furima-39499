f99d9b9b5743512e83e87830af52fe7c1e030a95
Leebal13 <98734053+Leebal13@users.noreply.github.com>
2023-07-26 22:03:54 +0900

    deployです

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,14 +82,4 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-end
-
-server '54.248.130.187', user: 'ec2-user', roles: %w{app db web}
+  
