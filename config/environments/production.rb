14ed5d6aea5a64ee02aee58e7fdd588305bcb3d2
Leebal13 <98734053+Leebal13@users.noreply.github.com>
2023-07-26 20:20:53 +0900

    Unicornまで編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+server '54.248.130.187', user: 'ec2-user', roles: %w{app db web}
