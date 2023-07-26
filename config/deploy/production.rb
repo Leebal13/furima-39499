b649185254c5ff2a4bac13bbd77ad248809fbec0
Leebal13 <98734053+Leebal13@users.noreply.github.com>
2023-07-26 22:07:43 +0900

    copush

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,1 @@
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger    = ActiveSupport::TaggedLogging.new(logger)
-end
-
-# Do not dump schema after migrations.
-config.active_record.dump_schema_after_migration = false
-end
-
 server '54.248.130.187', user: 'ec2-user', roles: %w{app db web}
